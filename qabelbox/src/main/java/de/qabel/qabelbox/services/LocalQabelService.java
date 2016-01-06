8c07dc51d476f3af72c5b89575957372f1ac957a
Jens Kadenbach <gitdax@daxbau.net>
2016-01-06 15:04:07 +0100

    Updated BoxProviderTest
    The BoxProviderTest uses a subclass of a the BoxProvider which mocks
    out the LocalQabelService.

--- qabelbox/src/main/java/de/qabel/qabelbox/services/LocalQabelService.java
+++ qabelbox/src/main/java/de/qabel/qabelbox/services/LocalQabelService.java
@@ -5,42 +5,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 import org.spongycastle.util.encoders.Hex;
 
-import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import de.qabel.ackack.event.EventEmitter;
-import de.qabel.core.config.Account;
 import de.qabel.core.config.Contact;
 import de.qabel.core.config.Contacts;
-import de.qabel.core.config.DropServer;
 import de.qabel.core.config.Identities;
 import de.qabel.core.config.Identity;
-import de.qabel.core.config.LocalSettings;
-import de.qabel.core.config.LocaleModuleSettings;
 import de.qabel.core.config.Persistable;
-import de.qabel.core.config.Persistence;
 import de.qabel.core.config.ResourceActor;
-import de.qabel.core.config.Settings;
-import de.qabel.core.config.SyncedModuleSettings;
-import de.qabel.core.config.SyncedSettings;
-import de.qabel.core.crypto.BinaryDropMessageV0;
 import de.qabel.core.crypto.CryptoUtils;
 import de.qabel.core.drop.DropMessage;
-import de.qabel.core.drop.DropURL;
-import de.qabel.core.exceptions.QblDropPayloadSizeException;
 import de.qabel.core.exceptions.QblInvalidEncryptionKeyException;
-import de.qabel.core.http.DropHTTP;
-import de.qabel.core.http.HTTPResult;
-import de.qabel.qabelbox.QabelBoxApplication;
 import de.qabel.qabelbox.config.AndroidPersistence;
 import de.qabel.qabelbox.config.QblSQLiteParams;
 
@@ -58,18 +39,18 @@ public class LocalQabelService extends Service {
 
 	private final IBinder mBinder = new LocalBinder();
 
-	static final String DB_NAME = "qabel-service";
-	private static final int DB_VERSION = 1;
-	private AndroidPersistence persistence;
-	private SharedPreferences sharedPreferences;
+	protected static final String DB_NAME = "qabel-service";
+	protected static final int DB_VERSION = 1;
+	protected AndroidPersistence persistence;
+	SharedPreferences sharedPreferences;
 
-	private void setLastActiveIdentityID(String identityID) {
+	protected void setLastActiveIdentityID(String identityID) {
 		sharedPreferences.edit()
 				.putString(PREF_LAST_ACTIVE_IDENTITY, identityID)
 				.apply();
 	}
 
-	private String getLastActiveIdentityID() {
+	protected String getLastActiveIdentityID() {
 		return sharedPreferences.getString(PREF_LAST_ACTIVE_IDENTITY, "");
 	}
 
@@ -201,7 +182,11 @@ public class LocalQabelService extends Service {
 	public void onCreate() {
 		super.onCreate();
 		Log.i(TAG, "LocalQabelService created");
-		sharedPreferences = getSharedPreferences(this.getClass().getCanonicalName(), MODE_PRIVATE);
+		initSharedPreferences();
+		initAndroidPersistence();
+	}
+
+	protected void initAndroidPersistence() {
 		AndroidPersistence androidPersistence;
 		QblSQLiteParams params = new QblSQLiteParams(this, DB_NAME, null, DB_VERSION);
 		try {
@@ -211,6 +196,10 @@ public class LocalQabelService extends Service {
 			return;
 		}
 		this.persistence = androidPersistence;
+	}
+
+	protected void initSharedPreferences() {
+		sharedPreferences = getSharedPreferences(this.getClass().getCanonicalName(), MODE_PRIVATE);
 		if (!sharedPreferences.getBoolean(PREF_DEVICE_ID_CREATED, false)) {
 
 			CryptoUtils cryptoUtils = new CryptoUtils();
@@ -222,7 +211,6 @@ public class LocalQabelService extends Service {
 					.putBoolean(PREF_DEVICE_ID_CREATED, true)
 					.apply();
 		}
-
 	}
 
 	@Override
